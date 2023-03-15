266e2ae19782037732f45600bbd7c55e650477ca
HP <HP@LAPTOP-LPHC7IBS>
2023-03-15 13:00:16 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModels.cs
@@ -0,0 +1,22 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModels
+    {
+        public int Place { get; set; }
+        public string Date { get; set; }
+        public int Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
